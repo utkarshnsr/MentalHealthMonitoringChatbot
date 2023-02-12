0f61e3f55253becf423846a9c771904344aa5fcd
Utkarsh Nattamai Subramanian Rajkumar <38819418+utkarshnsr@users.noreply.github.com>
2023-02-12 18:56:30 -0500

    added previous and next question functionality

--- app/src/main/java/com/example/mentalhealth/MainActivity.java
+++ app/src/main/java/com/example/mentalhealth/MainActivity.java
@@ -4,21 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static int questionIndex = 0;
+    public static String[] questions = {"Tell me something interesting that happened to you today..","How are you feeling?","Are you excited about today?"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void startRecording(View view) {
-        Toast.makeText(this,"recording started!",Toast.LENGTH_SHORT).show();
+
+    public void getNextQuestion(View view) {
+        int nextIndex;
+        if (MainActivity.questionIndex == (MainActivity.questions.length - 1)) {
+            nextIndex = MainActivity.questions.length - 1;
+        } else {
+            nextIndex = Math.abs((MainActivity.questionIndex + 1)) % (MainActivity.questions.length);
+        }
+        TextView tv1 = (TextView)findViewById(R.id.questionTextView);
+        tv1.setText(questions[nextIndex]);
+        MainActivity.questionIndex = nextIndex;
     }
 
-    public void stopRecording(View view) {
-        Toast.makeText(this,"recording stopped!",Toast.LENGTH_SHORT).show();
+    public void getPreviousQuestion(View view) {
+        int previousIndex;
+        if (MainActivity.questionIndex == 0) {
+            previousIndex = 0;
+        } else {
+            previousIndex = Math.abs((MainActivity.questionIndex - 1)) % (MainActivity.questions.length);
+        }
+        TextView tv1 = (TextView)findViewById(R.id.questionTextView);
+        tv1.setText(questions[previousIndex]);
+        MainActivity.questionIndex = previousIndex;
     }
 }
