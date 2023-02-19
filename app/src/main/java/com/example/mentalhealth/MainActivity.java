7694c7e4e7f25c36d4c1848a812747eb78bb9165
Utkarsh Nattamai Subramanian Rajkumar <38819418+utkarshnsr@users.noreply.github.com>
2023-02-18 19:24:29 -0500

    added some parts of video recording feature

--- app/src/main/java/com/example/mentalhealth/MainActivity.java
+++ app/src/main/java/com/example/mentalhealth/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.mentalhealth;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
     public static int questionIndex = 0;
@@ -14,8 +19,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.recordVideoButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 1) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            VideoView videoView = new VideoView(this);
+            videoView.setVideoURI(data.getData());
+            videoView.start();
+            builder.setView(videoView).show();
+        }
+    }
 
     public void getNextQuestion(View view) {
         int nextIndex;
@@ -40,4 +65,6 @@ public class MainActivity extends AppCompatActivity {
         tv1.setText(questions[previousIndex]);
         MainActivity.questionIndex = previousIndex;
     }
+
+
 }
