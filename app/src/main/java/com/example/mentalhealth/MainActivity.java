2627b040285e119f71c8258991dbe24cf5290c7c
Utkarsh Nattamai Subramanian Rajkumar <38819418+utkarshnsr@users.noreply.github.com>
2023-02-20 14:17:40 -0500

    made latest changes

--- app/src/main/java/com/example/mentalhealth/MainActivity.java
+++ app/src/main/java/com/example/mentalhealth/MainActivity.java
@@ -31,7 +31,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Runnable {
     public static int questionIndex = 0;
     public static String[] questions = {"Tell me something interesting that happened to you today..","How are you feeling?","Are you excited about today?"};
 
@@ -39,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainActivity m1 = new MainActivity();
+        Thread connectToDB = new Thread(m1);
+        connectToDB.start();
+
 
         findViewById(R.id.recordVideoButton).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +53,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 1);
             }
         });
+    }
+
+    public void run() {
+        System.out.println("RUNNING SECOND THREAD");
+        try {
+            OkHttpClient client = new OkHttpClient().newBuilder().build();
+            MediaType mediaType = MediaType.parse("application/json");
+            RequestBody body = RequestBody.create(mediaType, "{\n    \"collection\":\"Questions\",\n    \"database\":\"videoRecords\",\n    \"dataSource\":\"MentalHealthCluster\",\n    \"projection\": {\"question\": 1}\n\n}");
+            Request request = new Request.Builder()
+                    .url("https://us-east-1.aws.data.mongodb-api.com/app/data-aibcf/endpoint/data/v1/action/find")
+                    .method("POST", body)
+                    .addHeader("Content-Type", "application/json")
+                    .addHeader("Access-Control-Request-Headers", "*")
+                    .addHeader("api-key", "8qP30Gmkf0Dij4dweva71Zh1ZpFVRP1GtdqJ8XKoXUAEVdKMbWO9h1NgyjY0kBWI")
+                    .build();
+            Response response = client.newCall(request).execute();
+            System.out.println(response.body().string());
+            System.out.println("Hi");
+        } catch (Exception e) {
+            System.out.println("INSIDE CATCH");
+            System.out.println(e);
+        }
     }
 
     @Override
