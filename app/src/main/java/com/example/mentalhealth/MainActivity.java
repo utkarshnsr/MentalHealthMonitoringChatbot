760fbeb6f7edff03b1d85984427f9de79bd75545
Utkarsh Nattamai Subramanian Rajkumar <38819418+utkarshnsr@users.noreply.github.com>
2023-02-20 11:51:52 -0500

    deleted mongodbexample file

--- app/src/main/java/com/example/mentalhealth/MainActivity.java
+++ app/src/main/java/com/example/mentalhealth/MainActivity.java
@@ -39,30 +39,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("BEFORE TRY");
-
-        try {
-            OkHttpClient client = new OkHttpClient().newBuilder().build();
-            MediaType mediaType = MediaType.parse("application/json");
-            RequestBody body = RequestBody.create(mediaType, "{\n    \"collection\":\"Questions\",\n    \"database\":\"videoRecords\",\n    \"dataSource\":\"MentalHealthCluster\",\n    \"projection\": {\"question\": 1}\n\n}");
-            Request request = new Request.Builder()
-                    .url("https://us-east-1.aws.data.mongodb-api.com/app/data-aibcf/endpoint/data/v1/action/find")
-                    .method("POST", body)
-                    .addHeader("Content-Type", "application/json")
-                    .addHeader("Access-Control-Request-Headers", "*")
-                    .addHeader("api-key", "FlALV8Uy8CjoZTNGMSRJevh7leeBWMfSmDGqsZzfrR83gUCgCUELAjxCNdN6PDO1")
-                    .build();
-            Response response = client.newCall(request).execute();
-            System.out.println("IN TRY");
-            System.out.println(response.body().string());
-        } catch (Exception e) {
-            System.out.println("IN EXCEPTION");
-            System.out.println(e);
-        }
-
-
-
-
 
         findViewById(R.id.recordVideoButton).setOnClickListener(new View.OnClickListener() {
             @Override
